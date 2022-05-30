329f2317b63d502255ad54c85164a7d9b243cac6
itmark <2403799616@qq.com>
2022-05-30 20:31:16 +0800

    完成了index首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,20 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
-  });
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  option.url = `http://www.liulongbin.top:3007` + option.url;
+  // 统一为有权限的接口，设置 headers 请求头
+  if (option.url.includes("/my/")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+  // option.complete = res=>{
+  //   if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+  //     //  强制清空 token
+  //     localStorage.removeItem("token");
+  //     // 强制跳转到登录页面
+  //     location.href = "/login.html"
+  // }
+  // }
+});
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token')
+        // },
+        success: res => {
+            // console.log(res);
+            if (res.status !== 0) return layer.msg("获取用户信息失败！");
+            layer.msg("获取用户信息成功！");
+            renderAvatar(res.data)
+            
+        },
+        // complete:res=>{
+        //     console.log(res);
+        // }
+    })
+}
+const renderAvatar = (user) => {
+    let uname = user.nickname || user.username
+    $('#welcome').html(`欢迎${uname}`)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr("src", user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        let firstName = uname[0].toUpperCase()
+        $('.text-avatar').html(firstName)
+    }
+}
+
+$('#btnLogout').on('click', () =>{
+    layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+})
+
+getUserInfo()
